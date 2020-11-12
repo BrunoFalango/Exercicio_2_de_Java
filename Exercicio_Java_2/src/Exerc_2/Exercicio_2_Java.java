24bdc6223a887b3bd619bf9f76416a0bab76bd07
BrunoFalango <brunofalango1997@gmail.com>
2020-11-12 15:04:26 -0300

    Exercicio 2 de java

---
+++ Exercicio_Java_2/src/Exerc_2/Exercicio_2_Java.java
@@ -0,0 +1,11 @@
+package Exerc_2;
+
+public class Exercicio_2_Java {
+
+	public static void main(String[] args) {
+		System.out.print("As habilidades que usei para chegar aqui foram: Persistencia, Coragem, \n"
+				+ "e usei tambem a habilidade de comunicação");
+
+	}
+
+}
